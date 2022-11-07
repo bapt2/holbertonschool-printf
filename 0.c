1910bee930808b9e9860d08cfdc223477728403f
bapt2 <5711@holbertonstudents.com>
2022-11-07 05:53:30 -0800

    printf

---
+++ 0.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_printf -function that produces output according to a format
+ *@format: is a character string
+ *Return: format
+ */
+int _printf(const char *format, ...)
+{
+	int i, j = 0;
+
+	for (i = 0; i != format[j]; i++)
+	{
+	}
+	return (i - 1);
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef PRINTF
+#define PRINTF
+#include <wchar.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+#endif

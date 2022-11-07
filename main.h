4c52060a58be8ceab003ae8eb1e4d3d0a76bfaaf
bapt2 <5711@holbertonstudents.com>
2022-11-07 07:35:08 -0800

    task 1

--- main.h
+++ main.h
@@ -4,10 +4,11 @@
 # include <unistd.h>
 # include <stdarg.h>
 
+void	_putnbr_runtime(long v, int *r);
 int	_putnbr(int v);
-
 int	_printf_char(va_list l);
 int	_printf_string(va_list l);
+int	_print_integer(va_list l);
 int	_printf(const char *format, ...);
 
 #endif

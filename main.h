d783624c9a207888c6b629af117e47ee37dba612
Snavyy <1337Snavy@gmail.com>
2022-11-07 15:21:40 +0100

    added files

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef PRINTF_H
 # define PRINTF_H
 
-# include <wchar.h>
 # include <unistd.h>
+# include <stdarg.h>
 
+int	_printf_char(va_list l);
+int	_printf_string(va_list l);
 int	_printf(const char *format, ...);
 
 #endif

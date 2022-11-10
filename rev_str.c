ae8003b30d51a621956372f2ec989b7e8dbe9955
bapt2 <5711@holbertonstudents.com>
2022-11-10 14:47:04 -0800

    fixed rev_str.c

--- rev_str.c
+++ rev_str.c
@@ -7,15 +7,24 @@
 void            _printf_handler_reversed_string(context_t *ctx)
 {
 	char	*v;
+	int	 x;
 
-	{
 	v = (char *) va_arg(ctx->l, char *);
 	if (!v)
+	{
 		v = ")null(";
 
-	context_write(ctx, v, _strlen(v));
-	ctx->i -= 1;
+		context_write(ctx, v, _strlen(v));
 	}
+	else
+	{
+		for (x = 0; v[x]; x++)
+	       		;
+		x--;
+		for (; x >= 0; x--)	
+			context_write(ctx, v, _strlen(v));
+	}	
+	ctx->i -= 1;
 _printf("%r", v);
 printf("%r", v);
 }

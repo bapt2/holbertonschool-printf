b65b9623cc68a972935a6602bbe1bd6c285d58d2
snavy <1337snavy@gmail.com>
2022-11-11 14:59:33 +0000

    fixed: rot13 case matching

--- rot_13.c
+++ rot_13.c
@@ -17,7 +17,7 @@ str	rot13(str s)
 		return (0);
 	for (x = 0; s[x]; x++)
 		if (_strchr(d, s[x]))
-			s[x] = d[((_strchr(d, s[x]) - d) > 26) * 26 + ((
+			s[x] = d[((_strchr(d, s[x]) - d) >= 26) * 26 + ((
 				_strchr(d, s[x]) - d) + 13) % 26];
 	return (s);
 }
